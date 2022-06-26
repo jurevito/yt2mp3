26c55f1d2d2b2b2632a287db6034b88860b85d77
Jure Vito Srovin <jure.vito@gmail.com>
2022-06-26 20:59:48 +0200

    Change repo to golang

---
+++ go.mod
@@ -0,0 +1,13 @@
+module yt2mp3
+
+go 1.18
+
+require github.com/kkdai/youtube/v2 v2.7.15
+
+require (
+	github.com/bitly/go-simplejson v0.5.0 // indirect
+	github.com/dlclark/regexp2 v1.4.1-0.20201116162257-a2a8dda75c91 // indirect
+	github.com/dop251/goja v0.0.0-20220516123900-4418d4575a41 // indirect
+	github.com/go-sourcemap/sourcemap v2.1.3+incompatible // indirect
+	golang.org/x/text v0.3.7 // indirect
+)
---
+++ main.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"io/fs"
+	"io/ioutil"
+
+	"github.com/kkdai/youtube/v2"
+)
+
+func main() {
+
+	link := "https://www.youtube.com/watch?v=jH1RNk8954Q&list=PL6YgdMS9Bn4GPY9Sm7BJtOwiVYzN-UINM&index=21"
+
+	client := youtube.Client{Debug: true}
+
+	video, err := client.GetVideo(link)
+	if err != nil {
+		panic(err)
+	}
+
+	// Typically youtube only provides separate streams for video and audio.
+	// If you want audio and video combined, take a look a the downloader package.
+	format := video.Formats.WithAudioChannels().FindByQuality("tiny")
+
+	reader, _, err := client.GetStream(video, format)
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Printf("bit rate: %v\n", format.AudioQuality)
+
+	// do something with the reader
+	content, err := io.ReadAll(reader)
+	if err != nil {
+		panic(err)
+	}
+
+	reader.Close()
+
+	fileName := fmt.Sprintf("./output/%s.mp3", video.Title)
+	err = ioutil.WriteFile(fileName, content, fs.ModePerm)
+	if err != nil {
+		panic(err)
+	}
+
+}
